0f2b0bc3f47940b66bf804b4fcf62c35dddd10e6
kvevyx <kiaraevvy@gmail.com>
2024-07-04 00:54:53 +0800

    configure website

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 document.addEventListener('DOMContentLoaded', (event) => {
     const form = document.getElementById('messageForm');
     const currentTime = document.getElementById('currentTime');
@@ -14,7 +42,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     updateTime();
     setInterval(updateTime, 1000);
 
-    // Handle form submission
+    // Handle form 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
 
@@ -25,3 +53,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
         displayPesan.textContent = formData.get('pesan');
     });
 });
+
